8b7c3cdf7281cc703308d179aac52243f81ee74b
jacobpalaoro <jpalaoro197@gmail.com>
2022-07-12 11:47:31 -0700

    all 4 perameters of TriangleType in Shapetracker.cs pass all tests in ShapetrackerTests.cs
    Co-authored-by: PJ Perry <pjperry28@gmail.com>

--- Shapetracker.Tests/ModelTests/ShapetrackerTests.cs
+++ Shapetracker.Tests/ModelTests/ShapetrackerTests.cs
@@ -7,22 +7,31 @@ namespace ShapeTracker.Tests
   public class ShapeTrackerTests
   {
      [TestMethod]
-    public void TriangleType_CheckToSeeIfTriangle_IsEquilateral()
+    public void TriangleType_CheckToSeeIfSidesAreEqual_Equilateral()
     {
-      Triangle testTriangle = new Triangle(1, 1, 1);
+      Triangle testTriangle = new Triangle(3, 3, 3);
       Assert.AreEqual("equilateral", testTriangle.TriangleType());
     }
+
     [TestMethod]
-    public void TriangleType_CheckToSeeIfTriangle_IsIsosceles()
+    public void TriangleType_CheckToSeeIf2SidesAreEqual_Isosceles()
     {
-      Triangle testTriangle = new Triangle(2, 2, 8);
+      Triangle testTriangle = new Triangle(3, 3, 1);
       Assert.AreEqual("isosceles", testTriangle.TriangleType());
     }
-     [TestMethod]
-    public void TriangleType_CheckToSeeIfTriangle_IsScalene()
+
+    [TestMethod]
+    public void TriangleType_CheckToSeeIfSidesNotEqual_Scalene()
     {
-      Triangle testTriangle = new Triangle(2, 4, 8);
+      Triangle testTriangle = new Triangle(4,3,5);
       Assert.AreEqual("scalene", testTriangle.TriangleType());
+    }
+
+    [TestMethod]
+    public void TriangleType_CheckToSeeIfTraingle_NotATriangle()
+    {
+      Triangle testTriangle = new Triangle(1, 2, 8);
+      Assert.AreEqual("not a triangle", testTriangle.TriangleType());
     }
   }
 }  
