7c011a923b2e9622376e8166eb9455f7fcb1f6f6
jacobpalaoro <jpalaoro197@gmail.com>
2022-07-12 11:12:42 -0700

    add return scalene as else statement.
    > Co-authored-by: PJ Perry <pjperry28@gmail.com>

---
+++ Shapetracker/Models/Shapetracker.cs
@@ -0,0 +1,29 @@
+namespace ShapeTracker
+{
+  public class Triangle
+  {
+    private int side1;
+    private int side2;
+    private int side3;
+
+    public Triangle(int _side1, int _side2, int _side3)
+    {
+      side1 = _side1;
+      side2 = _side2;
+      side3 = _side3;
+    }
+    public string TriangleType()
+    {
+     if (side1 == side2 && side2 == side3 && side1 == side3){
+        return "equilateral";
+      }
+      else if ( side1 == side2 || side2 == side3 || side1 == side3){
+        return "isosceles";
+      }
+      else
+      {
+        return "scalene";
+      }
+    }
+  }
+}
